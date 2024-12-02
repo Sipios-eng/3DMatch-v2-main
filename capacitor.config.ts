402b8783066e5d98ac66c2ae5dfc65af34abbfcb
Sipios-eng <art.moya@duocuc.cl>
2024-12-02 00:20:09 -0300

    capacitores para APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: '3DMatch',
   webDir: 'www'
 };
 
-export default {
-  appId: 'com.example.app',
-  appName: 'nombre-de-tu-app',
-  webDir: 'www',  // Asegúrate de que este valor esté correcto
-  bundledWebRuntime: false
-};
+export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBbGPqXmsf8p_Zuglcl3wlcAM6fV-h-P7M",
+    authDomain: "dmatch-78338.firebaseapp.com",
+    projectId: "dmatch-78338",
+    storageBucket: "dmatch-78338.appspot.com",
+    messagingSenderId: "812604593084",
+    appId: "1:812604593084:web:0630370ece077dd24b11b9"
+  }
 };
