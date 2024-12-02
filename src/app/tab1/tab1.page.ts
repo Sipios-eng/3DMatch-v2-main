da680d2dfb9444dda833be0e69143718ba048512
Sipios-eng <art.moya@duocuc.cl>
2024-12-02 12:16:05 -0300

    APK Y GROLOCALIZACVION

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Geolocation } from '@capacitor/geolocation';
 
 declare var google: any;  // Declarar la variable global de Google Maps
 
@@ -12,24 +12,48 @@ declare var google: any;  // Declarar la variable global de Google Maps
 export class Tab1Page implements OnInit {
   username: string = 'Invitado';
   userEmail: string = '';
+
   constructor(private route: ActivatedRoute) {}
-  ngOnInit() {this.loadMap();
+
+  ngOnInit() {
+    this.loadUsername();
+    this.loadMap();
     this.userEmail = localStorage.getItem('userEmail') || 'Usuario';
   }
+
   loadUsername() {
     const storedUsername = localStorage.getItem('username');
     if (storedUsername) {
       this.username = storedUsername;
     }
   }
-  
 
-  loadMap() {
-    const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-      center: { lat: -33.5138, lng: -70.5983 },  // Coordenadas iniciales
-      zoom: 16,
-    });
+  async loadMap() {
+    // Obtener la posición actual del usuario
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
 
-    // Puedes agregar más configuraciones aquí
+      const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: { lat: coordinates.coords.latitude, lng: coordinates.coords.longitude }, // Coordenadas actuales
+        zoom: 16,
+      });
+
+      // Añadir marcador en la ubicación del usuario
+      new google.maps.Marker({
+        position: { lat: coordinates.coords.latitude, lng: coordinates.coords.longitude },
+        map: map,
+        title: "Tu ubicación",
+      });
+
+    } catch (error) {
+      console.error('Error obteniendo la ubicación', error);
+
+      // En caso de error, cargar el mapa con coordenadas por defecto
+      const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: { lat: -33.5138, lng: -70.5983 },  // Coordenadas iniciales (Santiago)
+        zoom: 16,
+      });
+    }
   }
 }
